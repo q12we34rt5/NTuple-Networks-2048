3a93b0d6c83d96df1592abcf56e0082ba2c07042
q12we34rt5 <luke123132112@gmail.com>
2022-07-14 15:59:53 +0800

    Add github link.

--- components/App.vue
+++ components/App.vue
@@ -2,6 +2,24 @@
   <div ref="Tdl2048" class="Tdl2048">
     <div class="container">
       <div ref="game-container" class="game-container">
+        <div id="github-link">
+          <a href="https://github.com/q12we34rt5/NTuple-Networks-2048" target="_blank">
+            <svg
+              height="48"
+              aria-hidden="true"
+              viewBox="0 0 16 16"
+              version="1.1"
+              width="48"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+              ></path>
+            </svg>
+            <pre> </pre>
+            <span>q12we34rt5</span>
+          </a>
+        </div>
         <TdBoard ref="board" class="td-board" />
         <div class="reset-board" @click="onReset()">reset</div>
       </div>
@@ -209,5 +227,38 @@ export default {
 }
 .reset-board:hover {
   background: rgb(6, 187, 0);
+}
+#github-link {
+    left: 25%;
+    top: 10%;
+    width: 50%;
+    position: absolute;
+    text-align: center;
+    display: flex;
+    line-height: v-bind(layout.widthscale * 35 + "px");
+    font-size: v-bind(layout.widthscale * 35 + "px");
+    font-family: monospace;
+}
+#github-link>a {
+  text-align: center;
+  text-decoration: none;
+  margin: auto;
+  display: flex;
+  color: white;
+  text-shadow: v-bind(layout.widthscale * 2 + "px")
+    v-bind(layout.widthscale * 2 + "px") v-bind(layout.widthscale * 5 + "px")
+    #00000041;
+}
+#github-link>a>* {
+  margin: auto;
+}
+#github-link>a>svg {
+  position: relative;
+  background: white;
+  width: v-bind(layout.widthscale * 48 + "px");
+  height: v-bind(layout.widthscale * 48 + "px");
+  border: v-bind(1 + "px") solid white;
+  background: white;
+  border-radius: 50%;
 }
 </style>
